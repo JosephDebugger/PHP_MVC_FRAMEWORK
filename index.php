b923541406b1063c0ee6e366d4da7cfb04817971
Joseph Dias <josephdias012@gmail.com>
2022-09-01 22:31:16 +0600

    change

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/vendor/autoload.php';
+
 
 use Josep\Phpmvcframework\core\Application;
 
 $app = new Application();
 
-//hello
 $app->router->get('/', function(){
     return 'helloworld';
 });
