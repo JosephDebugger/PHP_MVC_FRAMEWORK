212cd9da6d8fbe4397268ccd0dc1cd901b1ecc61
Joseph Dias <josephdias012@gmail.com>
2022-09-01 22:24:00 +0600

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Josep\Phpmvcframework\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function(){
+    return 'helloworld';
+});
+$app->router->get('/contact', function(){
+    return 'contact';
+});
+
+$app->run();
